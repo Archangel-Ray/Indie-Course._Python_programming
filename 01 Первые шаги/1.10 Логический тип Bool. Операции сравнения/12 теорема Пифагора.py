516af2357961916fabaa79c96b0304c7de06300d
Sergey Savelyev <sergey.savelyev2020@gmail.com>
2024-04-10 11:56:37 +0300

    1.10 Логический тип Bool. Операции сравнения
    еслиб не конспектирование, наверно, в два раза быстрей было бы.
    но, от этого отказываться я не хочу.

---
+++ 01 Первые шаги/1.10 Логический тип Bool. Операции сравнения/12 теорема Пифагора.py
@@ -0,0 +1,20 @@
+"""
+На вход поступают три целых числа - стороны треугольника.
+
+Необходимо вывести True, если данные стороны образуют прямоугольный треугольник, в противном случае - False.
+
+Для написания программы необходимо вспомнить теорему Пифагора
+
+Сделать задачу необходимо без использования условного оператора.
+"""
+# a, b, c = map(int, input().split())
+# a, b, c = a ** 2, b ** 2, c ** 2
+# print(a + b == c or b + c == a or a + c == b)
+
+print(not True)
+print(not False)
+print(not True or False)
+print(not True or True)
+print(not (True or False))
+print(not True and not False)
+print(not (True and True))

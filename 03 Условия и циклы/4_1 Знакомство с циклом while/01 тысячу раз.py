bd8b15b084522b35d82fc5e16f8068396f85f4ba
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-12-11 09:46:13 +0300

    4.1 Знакомство с циклом while - повторить тысячу раз

---
+++ 03 Условия и циклы/4_1 Знакомство с циклом while/01 тысячу раз.py
@@ -0,0 +1,8 @@
+"""
+Напишите программу, которая распечатает все натуральные числа от 1000 до 2000 включительно.
+"""
+this_is_all = 2000
+that_s_not_all = 999
+while this_is_all != that_s_not_all:
+    that_s_not_all += 1
+    print(that_s_not_all)
